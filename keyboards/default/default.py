5d47369b817bdaf3d59700709dc573c52b19976b
okh-engineer <ohojimirzayev@gmail.com>
2023-10-18 14:41:28 +0500

    Add random book button

---
+++ keyboards/default/default.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+random_book = ReplyKeyboardMarkup(
+    keyboard = [
+        [KeyboardButton(text="🎲 Tasodifiy kitob")],
+    ],
+    resize_keyboard=True,
+)
