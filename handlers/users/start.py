2196e47b4d5aaa5636e02290a083043bba2daeef
okh-engineer <ohojimirzayev@gmail.com>
2023-10-20 23:19:06 +0500

    Move library command functions to commands.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,11 +1,10 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 import asyncpg
-import random
 from loader import dp, db, bot
 from data.config import ADMINS
 from keyboards.default.default import random_book
-import json
+
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     try:
@@ -22,23 +21,3 @@ async def bot_start(message: types.Message):
         pass
     await message.answer(f"Salom, {message.from_user.full_name}!", reply_markup=random_book)
     
-@dp.message_handler(commands=['library'])
-async def myCommand(message: types.Message):
-    user_telegram_id = message.from_user.id
-    
-    user_books = await db.get_favorite_books(telegram_id=user_telegram_id)
-
-    if user_books[0]:
-        user_books = json.loads(user_books[0])
-        keyboard = types.InlineKeyboardMarkup(row_width=1)
-        print(user_books)
-        for key, value in user_books.items():
-            text = f"{key}"
-            print(value)
-            button = types.InlineKeyboardButton(text=text, callback_data=f"my_books__{value}")
-            keyboard.insert(button)
-        
-        await message.answer("Sizning kutubxonangizda quyidagi kitoblar mavjud: ", reply_markup=keyboard)
-    else:
-        await message.answer("Sizning kutubxonangizda hali kitob yo'q")
-            
