9af52cf1d2b3f427f6637067818f83e69fd74c72
okh-engineer <ohojimirzayev@gmail.com>
2023-10-20 23:16:24 +0500

    Move library command functions to commands.py

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -1,10 +1,23 @@
 from loader import dp, db
 from aiogram import types
+import json
 
 @dp.message_handler(commands=['library'])
 async def myCommand(message: types.Message):
-    print(message)
     user_telegram_id = message.from_user.id
     
     user_books = await db.get_favorite_books(telegram_id=user_telegram_id)
-    print(user_books)
+
+    if user_books[0]:
+        user_books = json.loads(user_books[0])
+        keyboard = types.InlineKeyboardMarkup(row_width=1)
+        print(user_books)
+        for key, value in user_books.items():
+            text = f"{key}"
+            print(value)
+            button = types.InlineKeyboardButton(text=text, callback_data=f"my_books__{value}")
+            keyboard.insert(button)
+        
+        await message.answer("Sizning kutubxonangizda quyidagi kitoblar mavjud: ", reply_markup=keyboard)
+    else:
+        await message.answer("Sizning kutubxonangizda hali kitob yo'q")
