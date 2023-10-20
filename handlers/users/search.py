8ec2a0f8c3f18e5eeb3fee0e8f61238694424606
okh-engineer <ohojimirzayev@gmail.com>
2023-10-20 23:18:26 +0500

    move all functions to functions.py

--- handlers/users/search.py
+++ handlers/users/search.py
@@ -1,168 +1,14 @@
 from aiogram import types
 
 from loader import dp, db, bot
-import requests
-from urllib.parse import quote
-from pprint import pprint
-from data.config import TOKEN
-import random
 from aiogram.utils.exceptions import MessageNotModified
 from contextlib import suppress
 import json
 from aiogram.dispatcher import FSMContext
-from random import randrange
-
-# from keyboards.inline.inline import bookMenu
-import json
-# from psycopg2.extras import Json
+from .functions import get_keyboards, make_query, keyboard
+from pprint import pprint
 
 link = "http://mlibrary.up.railway.app/api/file-book-api/?"
-
-def make_query(link):
-    token = TOKEN
-    
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json",
-    }
-    
-    try:
-        response = requests.get(link, headers=headers)
-    except Exception as e:
-        print(e)
-        return False
-        
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f"Error code: {response.status_code}")
-
-
-@dp.message_handler(text="🎲 Tasodifiy kitob", state=None)
-async def send_random_book(message: types.Message):
-    # Bazaga so'rovni kamaytirish uchun va tezlikni oshirish uchun databaza dagi kitoblar soni local holatda kiritib qo'yildi
-        link = "http://mlibrary.up.railway.app/api/file-book-api/"
-    # count = make_query(link=link)
-    # if count:
-        bookMenu = types.InlineKeyboardMarkup(row_width=2)
-        random = randrange(456, 83715)
-        link = f"{link.split('?')[0]}{random}"
-        random_book = make_query(link=link)
-        if random_book:
-            favorites_btn = types.InlineKeyboardButton(text="❤️/💔", callback_data=f"favorites_btn__{random_book['file_link']}")
-            delete_mgs_btn = types.InlineKeyboardButton(text="❌", callback_data="delete_msg")
-            bookMenu.insert(favorites_btn)
-            bookMenu.insert(delete_mgs_btn)
-            await message.answer_document(caption=random_book['description'], document=random_book['file_link'], reply_markup=bookMenu)
-        else:
-            await message.answer(text="😥 Iltimos qayta urunib ko'ring")
-
-def get_books_data(query):
-    encoded_query = quote(query)
-
-
-    # URL
-    api_url = f"https://mlibrary.up.railway.app/api/file-book-api/?search={encoded_query}&status=True"
-
-    return make_query(api_url)
-
-
-def keyboard(books_data):
-    books = ["📔", "📕", "📖", "📗", "📘", "📙", "📚", "📑", "🔖", "🧾"]
-    rows = {
-            1: 1,
-            2: 2,
-            3: 3,
-            4: 4,
-            5: 5,
-            6: 6,
-            8: 4,
-            10: 5
-        }
-    
-    try:
-        keyboard = types.InlineKeyboardMarkup(row_width=rows[len(books_data['results'])])
-    except KeyError:
-        keyboard = types.InlineKeyboardMarkup(row_width=len(books_data['results']))
-        
-    
-    
-    if books_data['next']:
-        number_page = books_data['next'].split("?")[1][5:6]
-        try:
-            number_page = int(number_page)
-            if number_page > 2:
-                text = f"Natijalar {number_page-2}1-{number_page-1}0 {books_data['count']} tadan\n\n"
-            else:
-                text = f"Natijalar 1-10 {books_data['count']} tadan\n\n"
-        except Exception as e:
-            print("Error ", e)
-    
-    elif books_data['previous']:
-        number_page = books_data['previous'].split("?")[1][5:6]
-        try:
-            number_page = int(number_page)
-            if number_page > 1:
-                if books_data['next']:
-                    text = f"Natijalar {number_page+1}1-{number_page+2}0 {books_data['count']} tadan\n\n"
-                else:
-                    text = f"Natijalar {number_page}1-{books_data['count']} {books_data['count']} tadan\n\n"
-                    
-        except:
-            if books_data['next']:
-                text = f"Natijalar 20-30 {books_data['count']} tadan\n\n"
-            else:
-                text = f"Natijalar 10-{books_data['count']} {books_data['count']} tadan\n\n"
-                
-    else:
-        text = f"Natijalar 1-{books_data['count']} {books_data['count']} tadan\n\n"
-             
-    for i, book in enumerate(books_data['results'], start=1):
-        if book['document_filename']:
-            text += f"""{i}. <i>{book['document_filename'][0:60].strip()}</i>  {random.choice(books)}\n"""
-        else:
-            text += """{}. {}  {}\n""".format(i, book['description'][0:60].strip().replace('\n', ''), random.choice(books))
-            
-        button = types.InlineKeyboardButton(text=i, callback_data=f"book__{book['file_link']}")
-        keyboard.insert(button)
-    
-    if books_data['next']:
-        next_page_link = books_data['next'].split('?')[1]
-    else:
-        next_page_link = ''
-        
-    next_page_button = types.InlineKeyboardButton(text="⏭️", callback_data=f"next_page_link__{next_page_link}")
-        
-    if books_data['previous']:
-        prev_page_link = books_data['previous'].split('?')[1]
-    else: 
-        prev_page_link = ''
-        
-    prev_page_button = types.InlineKeyboardButton(text="⏮️", callback_data=f"prev_page_link__{prev_page_link}")
-
-    keyboard.insert(prev_page_button)
-    keyboard.insert(next_page_button)
-
-    return (keyboard, text)
-    
-def get_keyboards(query):
-    books_data = get_books_data(query=query)
-
-    
-    if books_data:
-        if books_data['count'] > 0:
-            keyboards = keyboard(books_data=books_data)
-            return (keyboards[1], 200, keyboards[0], books_data)
-            # await bot.send_message(text="Kitaplar:", reply_markup=keyboard)
-        else:
-            text = "🙇 Afsuski bunday kitob topilmadi, iltimos kitob nomini tekshirib ko'ring"
-            return (text, 404)
-    else:
-        text = "🙇 Iltimos qayta urunib ko'ring."
-        return (text, 405)
-    
-
 
 async def update_message(message: types.Message, new_value: str, keyboard: json):
     with suppress(MessageNotModified):
@@ -204,11 +50,15 @@ async def process_book_button(callback_query: types.CallbackQuery, state=FSMCont
                 bookMenu.insert(favorites_btn)
                 bookMenu.insert(delete_mgs_btn)
                 await callback_query.message.answer_document(caption=obj['description'].replace('\n', ''), document=book_link, reply_markup=bookMenu)
-                
+            else:
+                print("Obj datada yo'q")        
         await bot.answer_callback_query(callback_query.id)
+    else:
+        print("Data yo'q")
+        
 
 @dp.callback_query_handler(lambda callback_query: callback_query.data.startswith('next_page_'))
-async def change_page(query: types.CallbackQuery):
+async def change_page(query: types.CallbackQuery, state=FSMContext):
     if query.data.split('__')[1] == '':
         await query.answer(text="Sahifalar tugadi")
     
@@ -216,18 +66,20 @@ async def change_page(query: types.CallbackQuery):
         
         next_page_link = link + query.data.split('__')[1]
         data = make_query(next_page_link)
+        await state.update_data(data)
         keyboards = keyboard(data)
     
         await update_message(message=query.message, new_value=keyboards[1], keyboard=keyboards[0] )
 
 @dp.callback_query_handler(lambda callback_query: callback_query.data.startswith('prev_page_'))
-async def change_page(query: types.CallbackQuery):
+async def change_page(query: types.CallbackQuery, state=FSMContext):
 
     if query.data.split('__')[1] == "":
         await query.answer(text="Siz allaqachon birinchi sahifadasiz!")
     else:
         prev_page_link = link + query.data.split('__')[1]
         data = make_query(prev_page_link)
+        await state.update_data(data)
         keyboards = keyboard(data)
         
         await update_message(message=query.message, new_value=keyboards[1], keyboard=keyboards[0])
@@ -239,8 +91,7 @@ async def add_rm_fv_books(callback_query: types.CallbackQuery, state=FSMContext)
     document_link = callback_query.data.split('__')[1]
     
     user_books = await db.get_favorite_books(telegram_id=user_telegram_id)
-
-    # print(user_books['favorite_books'])
+    
     favorite_books = {document_filename : document_link}
     if user_books['favorite_books']:
         user_books = json.loads(user_books[0])
@@ -265,7 +116,6 @@ async def delete_msg(callback_query: types.CallbackQuery, state=FSMContext):
     message = callback_query.message
     
     await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    # await call.message.delete()
     # await call.answer(cache_time=60)
   
   
